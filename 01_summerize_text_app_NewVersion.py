ee91682e8b1907c698145a387f2b8e4907db636f
HyeongTaekJo <144185989+HyeongTaekJo@users.noreply.github.com>
2025-07-13 15:12:52 +0900

    Update 01_summerize_text_app_NewVersion.py

--- 01_summerize_text_app_NewVersion.py
+++ 01_summerize_text_app_NewVersion.py
@@ -1,56 +1,52 @@
 ##### 기본 정보 불러오기 ####
-# Streamlit 패키지 추가
 import streamlit as st
-# OpenAI 패키기 추가
 import openai
 
 ##### 기능 구현 함수 #####
-def askGpt(prompt,apikey):
-    client = openai.OpenAI(api_key = apikey)
+def askGpt(text, apikey):
+    system_prompt = '''
+    You are an expert assistant that summarizes text into **Korean language**.
+    Your summaries should include the following:
+    - Omit duplicate content, but increase the summary weight of duplicate content.
+    - Summarize by emphasizing concepts and arguments rather than case evidence.
+    - Summarize in 3 lines.
+    - Use the format of a bullet point.
+    '''
+
+    client = openai.OpenAI(api_key=apikey)
     response = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[{"role": "user", "content": prompt}])
-    gptResponse = response.choices[0].message.content
-    return gptResponse
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": text}
+        ]
+    )
+    return response.choices[0].message.content
 
 ##### 메인 함수 #####
 def main():
     st.set_page_config(page_title="요약 프로그램")
-    # session state 초기화
+
     if "OPENAI_API" not in st.session_state:
         st.session_state["OPENAI_API"] = ""
 
     # 사이드바
     with st.sidebar:
-        # Open AI API 키 입력받기
         open_apikey = st.text_input(
             label='OPENAI API 키', 
             placeholder='Enter Your API Key', 
-            value='',type='password',
-        )    
-
-        # 입력받은 API 키 표시
+            value='', type='password',
+        )
         if open_apikey:
             st.session_state["OPENAI_API"] = open_apikey
         st.markdown('---')
 
     st.header("📃요약 프로그램")
     st.markdown('---')
-    
-    text = st.text_area("요약 할 글을 입력하세요")
-    if st.button("요약"):
-        prompt = f'''
-        **Instructions** :
-        - You are an expert assistant that summarizes text into **Korean language**.
-        - Your task is to summarize the **text** sentences in **Korean language**.
-        - Your summaries should include the following :
-            - Omit duplicate content, but increase the summary weight of duplicate content.
-            - Summarize by emphasizing concepts and arguments rather than case evidence.
-            - Summarize in 3 lines.
-            - Use the format of a bullet point.
-        -text : {text}
-        '''
-        st.info(askGpt(prompt,st.session_state["OPENAI_API"]))
 
-if __name__=="__main__":
+    text = st.text_area("요약 할 글을 입력하세요")
+    if st.button("요약") and text.strip():
+        st.info(askGpt(text, st.session_state["OPENAI_API"]))
+
+if __name__ == "__main__":
     main()
